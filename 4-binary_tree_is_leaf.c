06ca252ed14079e7b7fd15cc05f542b59060abfa
Entwoane <antoine.musci@gmail.com>
2024-12-11 12:44:23 +0100

    Function that checks if node is a leaf. Correcting return values

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,18 +1,17 @@
 #include "binary_trees.h"
-#include <stdlib.h>
-
 /**
- * binary_tree_insert_left - inserts a node as the left child of another node
- * @parent: pointer to the parent node
- * @value: value to store in the new node
- * Return: return a pointer to the new node, or NULL on failure
+ * binary_tree_is_leaf - Function that checks if a node is a leaf
+ * @node: Pointer to the node to check
+ * Return: 1 if node is a leaf
+ * Return 0 if its not
  */
-
 int binary_tree_is_leaf(const binary_tree_t *node)
-
 {
-	 if (node == NULL)
-        return (0);
+	if (node == NULL)
+		return (0);
 
-    return (node->left == NULL && node->right == NULL);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
 }
